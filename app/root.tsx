afb6b013b59bec8fcd3c1ecf3b3f82606425702c
Hữu Được <nhd.ars@gmail.com>
2024-04-10 07:58:13 +0700

    update package

--- app/root.tsx
+++ app/root.tsx
@@ -6,7 +6,6 @@ import {
 } from "@remix-run/node";
 import {
   Links,
-  LiveReload,
   Meta,
   Outlet,
   Scripts,
@@ -163,7 +162,6 @@ export default function App() {
           </button>
         ) : null}
         <Scripts />
-        <LiveReload />
       </body>
     </html>
   );
